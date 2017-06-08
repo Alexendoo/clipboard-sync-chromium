59f438c8bfe8014230943e8ca7cb6abb2cd9e19c
Alex Macleod <alex@macleod.io>
2017-06-08 11:08:07 +0100

    use URL to join paths

--- src/http.ts
+++ src/http.ts
@@ -1,13 +1,13 @@
 import { Config } from './state'
 
 export async function get(config: Config, path: string) {
-  const target = config.server + path
-  return fetch(target)
+  const target = new URL(path, config.server)
+  return fetch(target.href)
 }
 
 export async function post(config: Config, path: string, body: Uint8Array) {
-  const target = config.server + path
-  return fetch(target, {
+  const target = new URL(path, config.server)
+  return fetch(target.href, {
     body,
     method: 'POST',
   })
